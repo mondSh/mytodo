0a6d41c494b48af07e601652b88335c511251ad5
oren <mondshein@gmail.com>
2018-03-17 14:49:09 +0200

    add router and fix a little bug

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import {render} from 'react-dom';
 import {Provider} from 'react-redux';
 import App from './app/containers/App';
 import configureStore from './app/store/configureStore';
+import {Router, Route, browserHistory} from 'react-router';
 
 import 'todomvc-app-css/index.css';
 
@@ -12,7 +13,9 @@ const store = configureStore();
 
 render(
   <Provider store={store}>
-    <App/>
+    <Router history={browserHistory}>
+      <Route path="/" component={App}/>
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
